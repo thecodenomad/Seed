8ad9e380c8802aa7330e1d677573adcb5dd161ba
Ray Gomez <ray.rgomez@gmail.com>
2024-09-10 02:47:57 -0700

    More tweaks to build environment.

--- seed/__init__.py
+++ seed/__init__.py
@@ -1,2 +1,5 @@
-__version__ = "0.1.0-post.4+52c10b0"
-__version_tuple__ = (0, 1, 0, "post", 4, "52c10b0")
+"""Base app"""
+
+from importlib_metadata import version
+
+__version__ = version(__package__)
