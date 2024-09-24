eeb7ff05574b4ff335d41f7b1221de8546179ab2
Ray G. <ray.rgomez@gmail.com>
2024-09-24 06:35:39 +0000

    UI addition (#5)
    * Add UI pieces and update hooks.
    * Workflow update.

---
+++ src/view/window.py
@@ -0,0 +1,12 @@
+from gi.repository import Adw
+from gi.repository import Gtk
+
+
+@Gtk.Template(resource_path="/org/thecodenomad/seeds/window.ui")
+class SeedsWindow(Adw.ApplicationWindow):
+    __gtype_name__ = "SeedsWindow"
+
+    label = Gtk.Template.Child()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
